fb529290b10abd3feeef7cc4bb7fe6d5341ad369
kotkapitan <kapitankott@gmail.com>
2022-07-03 15:36:21 +0200

    first commit

---
+++ EtherWallet.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract EtherWallet {
+
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "Caller is not an owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
